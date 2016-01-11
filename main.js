bcc26472c785689c99c42e712dafdcb2fc11be54
mig21bb <mig21bb@gmail.com>
2016-01-11 19:50:03 +0100

    2016/01/11

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 var element = document.querySelector("#greeting");
 element.innerText = "Hello, world!";
 
+function getlist(file) {
+var xmlhttp = new XMLHttpRequest();
+var url = file;
+xmlhttp.open("GET", url, false);
+xmlhttp.send();
+console.log('After send ' + xmlhttp.responseText);
+var myArr = JSON.parse(xmlhttp.responseText);
+return myArr;
+}
+
+
+var stocklist = getlist("companies.json");
+var marketlist = getlist("markets.json");
+var dividendlist = getlist("dividends.json");
+var operationlist = getlist("operations.json");
+console.log('stocklist =' + stocklist);
+
 var listingstocks = document.querySelector("#stocklist");
 var listingmarkets = document.querySelector("#marketlist");
 var listingdividends = document.querySelector("#dividendlist");
 
-for (i = 0; i < 8; i++) {
-    listingstocks.innerHTML += "<li> Stock " + i + "</li>";
+function listelements (face,list) {
+
+for (i = 0; i < 3; i++) {
+  for (j = 0; j < 2; j++) {
+    face.innerHTML += "<li>" + list[i][j] +" ticker "+ list[i][j] + "</li>";
+  
+    }
+  }
 }
 
-for (i = 0; i < 6; i++) {
-    listingmarkets.innerHTML += "<li> Market " + i + "</li>";
-}
+listelements (listingstocks,stocklist);
 
-for (i = 0; i < 5; i++) {
-    listingdividends.innerHTML += "<li> dividend " + i + "</li>";
-}
+
+
+
