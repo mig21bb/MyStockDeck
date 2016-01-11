edc70d8abee49a0b8c03c084ed802ba1e3274968
mig21bb <mig21bb@gmail.com>
2016-01-11 10:38:45 +0100

    2016/01/11

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 var element = document.querySelector("#greeting");
 element.innerText = "Hello, world!";
+
+var listingstocks = document.querySelector("#stocklist");
+var listingmarkets = document.querySelector("#marketlist");
+var listingdividends = document.querySelector("#dividendlist");
+
+for (i = 0; i < 8; i++) {
+    listingstocks.innerHTML += "<li> Stock " + i + "</li>";
+}
+
+for (i = 0; i < 6; i++) {
+    listingmarkets.innerHTML += "<li> Market " + i + "</li>";
+}
+
+for (i = 0; i < 5; i++) {
+    listingdividends.innerHTML += "<li> dividend " + i + "</li>";
+}
